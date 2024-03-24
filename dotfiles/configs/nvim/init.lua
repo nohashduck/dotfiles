63bf509c654e267a7c9852fd41486e824c3039a0
nohashduck <153915439+nohashduck@users.noreply.github.com>
2024-03-24 21:26:12 +0300

    Update init.lua

--- dotfiles/configs/nvim/init.lua
+++ dotfiles/configs/nvim/init.lua
@@ -122,7 +122,6 @@ end
 function lsp()
 	return {
 		"VonHeikemen/lsp-zero.nvim",
-		
 		dependencies = {
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
@@ -132,19 +131,15 @@ function lsp()
 			"L3MON4D3/LuaSnip",
 		},
 
-		keys = {
-			{ "<leader>m", "<cmd>Mason<cr>" }
-		},
-
 		config = function()
 			local lsp_zero = require("lsp-zero");
-			
+
 			lsp_zero.on_attach(
 				function(client, bufnr)
 					lsp_zero.default_keymaps({buffer = bufnr})
 				end
 			)
-			
+
 			local signs = {
 				Error = "",
 				Warn = "",
@@ -156,22 +151,24 @@ function lsp()
 				local hl = "DiagnosticSign" .. type
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl });
 			end
-			
+
 			require("cmp").setup({
 				window = {
 					completion = require("cmp").config.window.bordered(),
 					documentation = require("cmp").config.window.bordered()
-				},
-
-				mapping = {
-      				["<Tab>"] = require("cmp").mapping(require("cmp").mapping.select_next_item(), { "i", "s" }),
-      				["<S-Tab>"] = require("cmp").mapping(require("cmp").mapping.select_prev_item(), { "i", "s" }),
-					["<CR>"] = require("cmp").mapping.confirm({ select = true }),
-				},
+				}
 			});
 
 			require('mason').setup({})
-			require('mason-lspconfig').setup({});
+			require('mason-lspconfig').setup({
+				handlers = {
+					lsp_zero.default_setup,
+					lua_ls = function()
+				  		local lua_opts = lsp_zero.nvim_lua_ls()
+				  		require('lspconfig').lua_ls.setup(lua_opts)
+					end
+			  	}
+			});
 		end
 	};
 end
