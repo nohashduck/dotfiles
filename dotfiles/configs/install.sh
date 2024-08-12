dd7e99d4eed668e7087f58c0a70e5a4d3c4e5b8d
nohashduck <167805533+nohashduck@users.noreply.github.com>
2024-08-12 23:22:58 +0300

    Update install.sh

--- dotfiles/configs/install.sh
+++ dotfiles/configs/install.sh
@@ -1,10 +1,4 @@
-sudo pacman -Syu git lazygit openssh zsh grub neofetch imagemagick neovim nodejs ranger htop kitty wlroots seatd sway swaybg waybar fuzzel wl-clipboard grim libnotify mako alsa-lib alsa-utils pulseaudio pulsemixer ttc-iosevka ttf-iosevka-nerd
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd ..
-rm -rf yay
-yay -S vieb-bin
+sudo pacman -Syu git firefox lazygit openssh zsh grub neofetch imagemagick neovim nodejs ranger htop kitty wlroots seatd sway swaybg waybar fuzzel wl-clipboard grim libnotify mako alsa-lib alsa-utils pulseaudio pulsemixer ttc-iosevka ttf-iosevka-nerd
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
