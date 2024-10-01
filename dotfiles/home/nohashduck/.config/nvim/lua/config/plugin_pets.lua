f3ebae9eeb6bd3d3ad99019e50a10587ac39d743
nohashduck <167805533+nohashduck@users.noreply.github.com>
2024-10-01 15:48:46 +0300

    Create plugin_pets.lua

---
+++ dotfiles/home/nohashduck/.config/nvim/lua/config/plugin_pets.lua
@@ -0,0 +1,18 @@
+return {
+    "giusgad/pets.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "giusgad/hologram.nvim",
+    },
+
+    config = function()
+        require("pets").setup {
+            row = 5,
+            speed_multiplier = 1,
+            death_animation = true,
+            popup = {
+                width = "25%",
+            },
+        };
+    end
+}
